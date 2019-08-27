9871dca4a6604335b6f86c4f038e9283938b3c82
Pancor <pancordev@gmail.com>
2019-08-27 20:10:22 +0200

    Find dominant color of table.

--- app/src/main/java/pl/pancordev/opencvsample/FindTableActivity.kt
+++ app/src/main/java/pl/pancordev/opencvsample/FindTableActivity.kt
@@ -1,14 +1,14 @@
 package pl.pancordev.opencvsample
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.drawable.Drawable
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.act_find_table.*
-import kotlinx.android.synthetic.main.act_find_table.imageView
-import kotlinx.android.synthetic.main.act_load_image.*
 import org.opencv.android.Utils
 import org.opencv.core.*
 import org.opencv.imgproc.Imgproc
@@ -17,15 +17,32 @@ import org.opencv.imgproc.Imgproc.*
 
 class FindTableActivity : AppCompatActivity() {
 
+    private val KEY = 100
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_find_table)
 
-        thresholding()
+        thresholding(null)
+
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/"
+        startActivityForResult(intent, KEY)
     }
 
-    //TODO: not finished
-    private fun findingDominantColor() {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == KEY && resultCode == Activity.RESULT_OK) {
+            if (data == null) return
+            val stream = applicationContext.contentResolver.openInputStream(data.data)
+            val bitmap = BitmapFactory.decodeStream(stream)
+            thresholding(bitmap)
+        }
+    }
+
+    private fun findingDominantColor(): Double {
         val tableBitmap = BitmapFactory.decodeResource(applicationContext.resources,
             R.drawable.table)
         val tableMat = Mat()
@@ -38,14 +55,15 @@ class FindTableActivity : AppCompatActivity() {
         cvtColor(resized, hsv, COLOR_BGR2HSV)
 
         val hist = Mat()
+        calcHist(listOf(hsv), MatOfInt(0), Mat(), hist, MatOfInt(180), MatOfFloat(0f, 180f))
 
-        calcHist(listOf(hsv), MatOfInt(0), Mat(), hist, MatOfInt(255), MatOfFloat(0f, 255f))
+        val minMaxLocResult = Core.minMaxLoc(hist)
+        return minMaxLocResult.maxLoc.y
     }
 
-    private fun thresholding() {
+    private fun thresholding(bit: Bitmap?) {
         //converting bitmap to Mat
-        val tableBitmap = BitmapFactory.decodeResource(applicationContext.resources,
-            R.drawable.table)
+        val tableBitmap = bit ?: BitmapFactory.decodeResource(applicationContext.resources, R.drawable.table)
         val tableMat = Mat()
         Utils.bitmapToMat(tableBitmap, tableMat)
 
@@ -55,15 +73,23 @@ class FindTableActivity : AppCompatActivity() {
         resize(tableMat, resized, Size(300.0, 150.0))
         val ratio = tableMat.size().width / resized.size().width
 
-        val gray = Mat()
-        cvtColor(resized, gray, COLOR_BGR2GRAY)
+        val hsv = Mat()
+        cvtColor(resized, hsv, COLOR_BGR2HSV)
         val blurred = Mat()
-        GaussianBlur(gray, blurred, Size(5.0, 5.0), 0.0)
+        GaussianBlur(hsv, blurred, Size(5.0, 5.0), 0.0)
         val thresh = Mat()
 //        adaptiveThreshold(blurred, thresh, 255.0, ADAPTIVE_THRESH_GAUSSIAN_C,
 //            THRESH_BINARY, 7, 2.0)
-        threshold(blurred, thresh, 60.0, 255.0, THRESH_BINARY)
+//        threshold(blurred, thresh, 169.0, 255.0, THRESH_BINARY)
 
+
+        val hist = Mat()
+        calcHist(listOf(blurred), MatOfInt(0), Mat(), hist, MatOfInt(180), MatOfFloat(0f, 180f))
+
+        val minMaxLocResult = Core.minMaxLoc(hist)
+        val hColor = minMaxLocResult.maxLoc.y
+
+        Core.inRange(blurred, Scalar(hColor - 10, 0.0, 0.0), Scalar(hColor + 10, 255.0, 255.0), thresh)
         setComparinImage(thresh)
 
         val cnts: List<MatOfPoint> = mutableListOf()
@@ -87,24 +113,30 @@ class FindTableActivity : AppCompatActivity() {
             }
         }
 
-        val rec = boundingRect(cnts[maxIndex])
+        if (cnts.isNotEmpty()) {
+            val rec = boundingRect(cnts[maxIndex])
 
-        //TODO: minAreaRect
+            //TODO: minAreaRect
 
-        val points = cnts[maxIndex].toArray()
-        val rotatedRect = minAreaRect(MatOfPoint2f(*points))
+            val points = cnts[maxIndex].toArray()
+            val rotatedRect = minAreaRect(MatOfPoint2f(*points))
 
-        val rectPoints = Array(4) { Point() }
-        rotatedRect.points(rectPoints)
-        drawContours(resized, listOf(MatOfPoint(*rectPoints)), 0, Scalar(255.0,
-            0.0, 0.0), Core.FILLED)
+            val rectPoints = Array(4) { Point() }
+            rotatedRect.points(rectPoints)
+            drawContours(
+                resized, listOf(MatOfPoint(*rectPoints)), 0, Scalar(
+                    255.0,
+                    0.0, 0.0
+                ), Core.FILLED
+            )
 
-        //rectangle(resized, rec.br(), rec.tl(),Scalar(255.0, 0.0, 0.0), Core.FILLED)
+            //rectangle(resized, rec.br(), rec.tl(),Scalar(255.0, 0.0, 0.0), Core.FILLED)
 
 //        drawContours(resized, listOf(cnts[maxIndex]), 0, Scalar(255.0,
 //            0.0, 0.0), 3)
 
-        setImageResult(resized)
+            setImageResult(resized)
+        }
 
 //        val points = MatOfPoint2f(tableMat)
 //        val peri = Imgproc.arcLength(points, true)
